50920816f7e58b0cfc2de6bc5618fed1d703a60c
Abdul Rahman SABBAGH <abbddos@gmail.com>
2019-08-12 21:13:19 +0300

    Update migrate.py

--- migrate.py
+++ migrate.py
@@ -2,5 +2,5 @@ import os
 import EnterpriseConfig
 
 os.chdir('/home/abdul/enterprise/model')
-MigrateCMD = 'psql -U {} -d postgres -a -f Model.sql'.format(EnterpriseConfig.dbRoot)
+MigrateCMD = 'psql -U {} -d postgres -a -f Model.sql'.format(EnterpriseConfig.dbusername)
 os.system(MigrateCMD)
