b3b9324f2fd24c40df349e8f29b265c910a1041d
abdul rahman <abbddos@gmail.com>
2020-08-19 01:16:55 +0300

    ...

--- EnterpriseConfig.py
+++ EnterpriseConfig.py
@@ -4,3 +4,4 @@ Email_Username = 'abbddos@gmail.com'	#Enter the email you'd like to use here.
 Email_Password = 'AbdulRahman*SS1983'	#Enter the email password that you'd like to use here.
 dbusername = 'abdul'		#Enter the database root username here
 dbpassword = 'ar*big1980'		#Enter the database root password here
+SecretKey = 'fhlvtubkbbwdqpmoultecyzjzdkojkdc'
