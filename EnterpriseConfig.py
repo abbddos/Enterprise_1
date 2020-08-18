50170eb227ce9d5fb0679fce27593bc7ee5f1380
Abdul Rahman SABBAGH <abbddos@gmail.com>
2020-08-19 01:18:58 +0300

    Update EnterpriseConfig.py

--- EnterpriseConfig.py
+++ EnterpriseConfig.py
@@ -1,7 +1,7 @@
 Server = 'localhost'		#Enter your server address here
-Mail_Server = 'smtp.gmail.com'
-Email_Username = 'abbddos@gmail.com'	#Enter the email you'd like to use here.
-Email_Password = 'AbdulRahman*SS1983'	#Enter the email password that you'd like to use here.
-dbusername = 'abdul'		#Enter the database root username here
-dbpassword = 'ar*big1980'		#Enter the database root password here
-SecretKey = 'fhlvtubkbbwdqpmoultecyzjzdkojkdc'
+Mail_Server = 'smtp.gmail.com' #Enter your Mail server as per the example here
+Email_Username = 'someemail@somedomain.com'	#Enter the email you'd like to use here.
+Email_Password = 'someemailpassword'	#Enter the email password that you'd like to use here.
+dbusername = 'somedatabaserootusername'		#Enter the database root username here
+dbpassword = 'somedatabaserootpassword'		#Enter the database root password here
+SecretKey = 'secretkey' #Enter your secret key here.
