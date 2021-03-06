8f1bf17d0d8d2f29a814384a8ac69eebb9076495
abbddos <abbddos@gmail.com>
2021-03-06 02:02:38 +0200

    Commit: 06 Mar 2021: 02:02 AM

--- EnterpriseConfig.py
+++ EnterpriseConfig.py
@@ -1,7 +1,7 @@
 Server = 'localhost'		#Enter your server address here
 Mail_Server = 'smtp.gmail.com' #Enter your Mail server as per the example here
-Email_Username = 'someemail@somedomain.com'	#Enter the email you'd like to use here.
-Email_Password = 'someemailpassword'	#Enter the email password that you'd like to use here.
-dbusername = 'databaserootusername'		#Enter the database root username here
-dbpassword = 'databaseuserpassword'		#Enter the database root password here
-SecretKey = 'secretkey' #Enter your secret key here.
+Email_Username = 'abbddos@gmail.com'	#Enter the email you'd like to use here.
+Email_Password = 'AbdulRahman*SS1983'	#Enter the email password that you'd like to use here.
+dbusername = 'abdul'		#Enter the database root username here
+dbpassword = 'abdul*1983'		#Enter the database root password here
+SecretKey = 'ididnotrealywantusingthissecretkeyatallbutitisthere' #Enter your secret key here.
--- static/js/toggler.js
+++ static/js/toggler.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+  var MenuStatus = 0;
+  $('#menu-icon').click(function(){
+    $('.vertical-menu').animate({width: "toggle" }, 100);
+    if(MenuStatus == 0){
+      MenuStatus = 1;
+      $("form").css('margin-left','5%');
+      $("table").css('margin-left','5%');
+      $('.table-in-form').css('margin-left','0%');
+      $('#srch').css('margin-left','5%');
+      $('.search-in-form').css('margin-left','0%');
+    }
+
+    else{
+      MenuStatus = 0;
+      $("form").css('margin-left','-12%');
+      $("table").css('margin-left','-12%');
+      $('.table-in-form').css('margin-left','0%');
+      $('#srch').css('margin-left','-12%');
+      $('.search-in-form').css('margin-left','0%');
+    }
+  });
+
   $('#warehouse').click(function(){
     $(this).children('li').slideToggle();
   });
