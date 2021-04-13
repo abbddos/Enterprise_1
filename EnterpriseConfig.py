cb6f31e8c770fbfea9436793bc394443b13ee64e
Abdul Rahman SABBAGH <abbddos@gmail.com>
2021-04-13 17:12:02 +0300

    Update EnterpriseConfig.py
    Final Commit

--- EnterpriseConfig.py
+++ EnterpriseConfig.py
@@ -4,4 +4,4 @@ Email_Username = ''	#Enter the email you'd like to use here.
 Email_Password = ''	#Enter the email password that you'd like to use here.
 dbusername = ''		#Enter the database root username here
 dbpassword = ''		#Enter the database root password here
-SecretKey = '' #Enter your secret key here.
+SecretKey = '' #Enter your secret key here, secret key can be any random string of your choice.
